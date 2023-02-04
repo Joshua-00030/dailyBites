08598e9d6e894dccdfaf87a5830495a77946278b
Joshua Romero <ggc638@my.utsa.edu>
2023-02-04 13:23:02 -0600

    missed imports in prior push. added to home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,8 @@
 import './Home.css';
-
+import CalorieBar from '../CalorieBar/CalorieBar';
+import CalorieDisplay from '../CalorieDisplay/CalorieDisplay';
+import ItemBar from '../ItemBar/ItemBar';
+import ItemContainer from '../ItemContainer/ItemContainer';
 const Home = () => {
     return (
         <div className="home-container">
