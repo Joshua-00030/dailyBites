312cf071f1e85d2626f3d1dba04becc4669c789f
Joshua Romero <joshua.romero@my.utsa.edu>
2023-02-20 14:03:16 -0600

    swapping to pc. services do not request items properly

--- src/components/Log/Log.js
+++ src/components/Log/Log.js
@@ -3,36 +3,47 @@ import CalorieBar from '../CalorieBar/CalorieBar';
 import Tagbar from '../Tagbar/Tagbar';
 import ItemContainer from '../ItemContainer/ItemContainer';
 import Searchbar from '../Searchbar/Searchbar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import userItemService from '../../services/userItem';
 
 const exampleItems = [
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 1 }, 
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 2 }, 
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 3 }, 
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 4 }, 
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 5 }, 
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 6 }, 
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 7 }, 
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 8 }, 
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 9 }, 
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 10 }, 
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 11 }, 
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 12 }, 
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 13 }, 
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 14 }, 
+    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 1 },
+    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 2 },
+    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 3 },
+    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 4 },
+    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 5 },
+    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 6 },
+    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 7 },
+    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 8 },
+    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 9 },
+    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 10 },
+    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 11 },
+    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 12 },
+    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 13 },
+    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 14 },
     { name: 'hamburger', calories: 350, tags: ['lunch'], id: 15 }
 ]
 
 const Log = () => {
 
+    const [userItems, setUserItems] = useState([])
     const [filteredItems, setFilteredItems] = useState(exampleItems)
-     
+
+    useEffect(() => {
+        userItemService
+            .getAll()
+            .then(initialItems => {
+                setUserItems(initialItems)
+            })
+    }, [])
+
+
     return (
         <>
             <div className="log-container">
-                <Searchbar items={exampleItems} setFilteredItems={setFilteredItems}/>
+                <Searchbar items={exampleItems} setFilteredItems={setFilteredItems} />
                 <Tagbar />
-                <ItemContainer items={filteredItems}/>
+                <ItemContainer items={filteredItems} />
                 <CalorieBar />
             </div>
         </>
