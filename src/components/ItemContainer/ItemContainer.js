d73ef06bbf5b0093a6c6c443a3a72ec4d7fe8385
AlexanderG310 <80711424+AlexanderG310@users.noreply.github.com>
2023-03-06 15:13:17 -0600

    Edited handleClick() & formatted
    No longer has undefined values or NaN on the Calorie Bar. Added useEffect() for debugging. Some formatting.

--- src/components/ItemContainer/ItemContainer.js
+++ src/components/ItemContainer/ItemContainer.js
@@ -1,13 +1,21 @@
+import React, { useEffect } from 'react';
 import UserItem from '../UserItem/UserItem'
 import './ItemContainer.css';
 
+
 const ItemContainer = ({items, currentCals, setCurrentCals}) => {
 
-    function handleClick(name, value){
-        setCurrentCals(prevCalories => prevCalories + value)
-        console.log("Item " + name + " has been clicked!\nThis food has " + value + " calories.");
-        console.log("Current Calory Total is: " + (currentCals + value));
+    function handleClick(item){
+        
+        const { name, nutrition } = item;
+        const calories = nutrition.find((nutrient) => nutrient.name === 'calories')?.value;
+        setCurrentCals(prevCalories => prevCalories + calories)
+        console.log(`Item ${name} has been clicked!\nThis food has ${calories} calories.`);
     }
+
+    useEffect(() => {
+        console.log(`Current Calory Total is: ${currentCals} `);
+    }, [currentCals]);
     
     return (
         <div className="item-grid-container">
@@ -16,8 +24,8 @@ const ItemContainer = ({items, currentCals, setCurrentCals}) => {
                     <UserItem  
                         key={item.id} 
                         item={item}
-                        onClick={() => handleClick(item.name, item.calories)}>
-                </UserItem>)}
+                        onClick={() => handleClick(item)}>
+                    </UserItem>)}
             </div>
         </div>
     )
--- src/components/Log/Log.js
+++ src/components/Log/Log.js
@@ -5,25 +5,6 @@ import ItemContainer from '../ItemContainer/ItemContainer';
 import Searchbar from '../Searchbar/Searchbar';
 import { useState, useEffect } from 'react';
 import userItemService from '../../services/userItem';
-/*
-const exampleItems = [
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 1 },
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 2 },
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 3 },
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 4 },
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 5 },
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 6 },
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 7 },
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 8 },
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 9 },
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 10 },
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 11 },
-    { name: 'oatmeal', calories: 150, tags: ['breakfast', 'low cal'], id: 12 },
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 13 },
-    { name: 'brown sugar oatmeal', calories: 150, tags: ['breakfast', 'low cal', 'oatmeal'], id: 14 },
-    { name: 'hamburger', calories: 350, tags: ['lunch'], id: 15 }
-]
-*/
 
 const Log = (props) => {
 
