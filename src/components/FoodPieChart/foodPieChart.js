b3d9750eb2172e63213c9bab7675387309068b76
Joshua Romero <joshua.romero@my.utsa.edu>
2023-03-31 21:39:08 -0500

    added nutrient view to the home page via a dropdown selector

--- src/components/FoodPieChart/foodPieChart.js
+++ src/components/FoodPieChart/foodPieChart.js
@@ -1,8 +1,12 @@
 import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts"
-const FoodPieChart = ({ className, data }) => {
+const FoodPieChart = ({ className, data, view }) => {
     const d = {}
     const d2 = []
+    const nutrientValue = {}
+    const nutrientData = []
+    const selection = [d2,nutrientData]
     var total = 0
+
     if (data) {
         data.forEach((item) => {
             if (d.hasOwnProperty(item.name)) {
@@ -10,14 +14,22 @@ const FoodPieChart = ({ className, data }) => {
             } else {
                 d[item.name] = item.nutrition[0].value
             }
-            total += item.nutrition[0].value
-        })
+                total += item.nutrition[0].value
+                item.nutrition.slice(1).forEach((nutrient) =>{
+                    if (nutrientValue.hasOwnProperty(nutrient.name)) {
+                        nutrientValue[nutrient.name] = nutrientValue[nutrient.name] += nutrient.value
+                    } else {
+                        nutrientValue[nutrient.name] = nutrient.value
+                    }
 
-        for (var item in d) {
-            d2.push({ name: item, value: d[item] })
+                })
+            })
+            for (var nutrient in nutrientValue) {
+                nutrientData.push({ name: nutrient, value: nutrientValue[nutrient] })
         }
-
-
+            for (var item in d) {
+                d2.push({ name: item, value: d[item] })
+        }
     }
 
     const stringToColour = (str) => {
@@ -44,7 +56,7 @@ const FoodPieChart = ({ className, data }) => {
                         border: "1px solid #cccc"
                     }}
                 >
-                    {d2.length > 0 ?
+                    {selection[view].length > 0 ?
                     <>
                     <label>{`${payload[0].name} : ${(payload[0].value / total * 100).toFixed(2)}%`}</label><br />
                     <label>{`Calories : ${payload[0].value}`}</label>
@@ -59,7 +71,7 @@ const FoodPieChart = ({ className, data }) => {
     return (
         <PieChart width={730} height={300}>
             <Pie
-                data={(d2.length > 0 ? d2 : [{ name: 'empty', value: 1 }])}
+                data={(selection[view].length > 0 ? selection[view] : [{ name: 'empty', value: 1 }])}
                 color="#000000"
                 dataKey="value"
                 nameKey="name"
@@ -70,7 +82,7 @@ const FoodPieChart = ({ className, data }) => {
                 outerRadius={120}
                 fill="#8884d8"
             >
-                {d2.length > 0 ?<>{d2.map((entry, index) => (
+                {selection[view].length > 0 ?<>{selection[view].map((entry, index) => (
                     <Cell
                         key={`cell-${index}`}
                         fill={stringToColour(entry.name)}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,38 +10,43 @@ import CalWidget from '../CalCalcWidget/CalWidget';
 const Home = () => {
 
     const [selectedWidget, setSelectedWidget] = useState(0);
+    const [view, setView] = useState(0);
     const [dailyData, setDailyData] = useState(null)
-    const [dateRange, setDateRange] = useState({sd: new Date(new Date().setHours(-24*7,0,0,0)).toISOString().split('T')[0], ed: new Date(new Date().setHours(0,0,0,0)).toISOString().split('T')[0]})
+    const [dateRange, setDateRange] = useState({ sd: new Date(new Date().setHours(-24 * 7, 0, 0, 0)).toISOString().split('T')[0], ed: new Date(new Date().setHours(0, 0, 0, 0)).toISOString().split('T')[0] })
 
     useEffect(() => {
-        console.log(dateRange.sd)
         const sdate = new Date(new Date(dateRange.sd).setMinutes(new Date(dateRange.sd).getTimezoneOffset()))
         const edate = new Date(new Date(dateRange.ed).setMinutes(new Date(dateRange.ed).getTimezoneOffset())).setHours(23, 59, 59, 999)
         userService.getHistory({ sdate: new Date(sdate), edate: new Date(edate) }).then(items =>
             setDailyData(items))
     }, [dateRange.ed, dateRange.sd])
-    
+
     const handleWidgetClick = (value) => {
         setSelectedWidget(value);
     }
 
     const widgetList = [
-        <FoodGraphChart className={"home-main-container"} data={dailyData}/>,
-        <FoodPieChart className={"home-main-container"} data={dailyData} />,
+        <FoodGraphChart className={"home-main-container"} data={dailyData} view={view}/>,
+        <FoodPieChart className={"home-main-container"} data={dailyData} view={view}/>,
         <CalWidget />,
         <div>Widget {selectedWidget}</div>,
         <div>Widget {selectedWidget}</div>]
-        
+
     return (
         <div className="home-page-container">
             <div className="home-grid-container">
                 <div className="home-main-container">
-                    {selectedWidget==2 ? "" : <div>
+                    {selectedWidget == 2 ? "" : <div>
                         <label for="start">Start date:</label>
                         <input type="date" id="start" name="trip-start" value={dateRange.sd} onChange={(event) => setDateRange({ sd: event.target.value, ed: dateRange.ed })} />
                         <br />
                         <label for="end">End date: </label>
                         <input type="date" id="end" name="trip-end" value={dateRange.ed} onChange={(event) => setDateRange({ sd: dateRange.sd, ed: event.target.value })} />
+                        <br/>
+                        <select name="view" id="view" onChange={(e) => setView(e.target.value)}>
+                            <option value="0">Calories</option>
+                            <option value="1">Nutrients</option>
+                        </select>
                     </div>}
                     {widgetList[selectedWidget]}
                 </div>
